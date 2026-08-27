a042f6ced1055651d691aca2f01c2f35c43f2070
Felipe Osorio <felipe.osorio.dev@gmail.com>
2026-08-27 04:32:38 -0300

    feat: Criação da classe Model que ira representar uma tabela no banco de dados.

---
+++ API/Models/Product/ProductModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Models.Product
+{
+    [Index(nameof(Ean), IsUnique = true)]
+    public class ProductModel
+    {
+        public long Id { get; set; }
+
+        [Column("Nome_Produto")]
+        public string Name { get; set; }
+
+        [Column("Cod_Produto")]
+        public string Ean { get; set; }
+
+        [Column("Quantidade")]
+        public int Amount { get; set; }
+        public bool IsEnable { get; set; }
+        public DateOnly CreatedAt { get; set; } = DateOnly.FromDateTime(DateTime.UtcNow);
+
+        [Column("Validade")]
+        public DateOnly Validate { get; set; }
+    }
+}
