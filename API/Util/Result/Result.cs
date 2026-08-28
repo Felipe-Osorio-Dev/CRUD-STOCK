19f73c855baa816a13dada08bb99ebdbb3dc7a76
Felipe Osorio <felipe.osorio.dev@gmail.com>
2026-08-28 04:50:39 -0300

    feat: Criado classe Result para lidar com os retornos dos metodos e padronizar o resultado.

---
+++ API/Util/Result/Result.cs
@@ -0,0 +1,30 @@
+﻿namespace API.Util.Result
+{
+    public class Result
+    {
+        public bool IsSuccess { get; }
+        public string MessageError { get; }
+
+        protected Result(bool isSuccess, string messageError)
+        {
+            IsSuccess = isSuccess;
+            MessageError = messageError;
+        }
+
+        public static Result Success() => new(true, string.Empty);
+        public static Result Failure(string messageError) => new(false, messageError);
+    }
+
+    public class Result<T> : Result
+    {
+        public T Data { get; }
+
+        private Result(bool isSuccess, string messageError, T data) : base(isSuccess, messageError)
+        {
+            Data = data;
+        }
+
+        public static Result<T> Success(T data) => new(true, string.Empty, data);
+        public static new Result<T> Failure(string messageError) => new(true, string.Empty, default!);
+    }
+}
