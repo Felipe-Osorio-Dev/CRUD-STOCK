809d8b0acf865e3a282b04d93bc5e617765726b8
Felipe Osorio <felipe.osorio.dev@gmail.com>
2026-08-30 00:30:31 -0300

    build: Adicionado Dependencia do Mapster ao projeto para mapeamento de dto e entidade.

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,7 @@
 using API.DataContext;
+using API.Repositorys.Product;
+using API.Services.Product;
+using Mapster;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +12,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddMapster();
+
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 var connectionString = builder.Configuration.GetConnectionString("AppConnectionString");
 builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
