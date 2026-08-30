b96509a01b47cdd61b2f9d90d9afb32aea4fb3f4
Felipe Osorio <felipe.osorio.dev@gmail.com>
2026-08-30 00:31:57 -0300

    feat: Criado DTOs de response para evitar expor o schema do banco.

---
+++ API/DTO/Responses/CreatedProductDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace API.DTO.Responses
+{
+    public class CreatedProductDTO
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Ean { get; set; }
+        public int Amount { get; set; }
+        public DateOnly Validate { get; set; }
+    }
+}
---
+++ API/DTO/Responses/ProductDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace API.DTO.Responses
+{
+    public class ProductDTO
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Ean { get; set; }
+        public int Amount { get; set; }
+        public DateOnly Validate { get; set; }
+    }
+}
