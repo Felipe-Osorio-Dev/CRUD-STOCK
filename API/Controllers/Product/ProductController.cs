611af63ad45889c772ecb15b4099a7f7c17bc5a3
Felipe Osorio <felipe.osorio.dev@gmail.com>
2026-08-31 05:00:51 -0300

    feat: Criado classe controller para expor os endpoints da api.

---
+++ API/Controllers/Product/ProductController.cs
@@ -0,0 +1,58 @@
+﻿using API.DTO.Requests;
+using API.DTO.Responses;
+using API.Services.Product;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace API.Controllers.Product
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductDTO>>> GetAllProductsAsync()
+        {
+            var list = await _productService.GetAllProductsAsync();
+
+            return Ok(list);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RegisterProductAsync([FromBody] RegisterProductDTO productDto)
+        {
+            var result = await _productService.RegisterProductAsync(productDto);
+
+            if(!result.IsSuccess)
+            {
+                return Conflict(result.MessageError);
+            }
+
+            return CreatedAtAction(
+                nameof(GetProductById),
+                new { id = result.Data.Id},
+                result.Data
+                );
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductDTO>> GetProductById(long id)
+        {
+            var result = await _productService.GetProductByIdAsync(id);
+
+            if(!result.IsSuccess)
+            {
+                return NotFound(result.MessageError);
+            }
+
+            return result.Data;
+        }
+    }
+}
