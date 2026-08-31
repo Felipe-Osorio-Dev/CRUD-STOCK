4ed40aeec3f2b8c909d7b803bd944d06486493e3
Felipe Osorio <felipe.osorio.dev@gmail.com>
2026-08-31 04:59:45 -0300

    feat: Criado classe repository para lidar diretamente com o db.

---
+++ API/Repositorys/Product/IProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using API.Models.Product;
+
+namespace API.Repositorys.Product
+{
+    public interface IProductRepository
+    {
+        Task RegisterProductAsync(ProductModel product);
+        Task<IEnumerable<ProductModel>> GetAllProductsAsync();
+        Task<ProductModel?> GetProductByIdAsync(long id);
+    }
+}
---
+++ API/Repositorys/Product/ProductRepository.cs
@@ -0,0 +1,32 @@
+﻿using API.DataContext;
+using API.Models.Product;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Repositorys.Product
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly AppDbContext _dbContext;
+
+        public ProductRepository(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<ProductModel>> GetAllProductsAsync()
+        {
+            return await _dbContext.Produtos.ToListAsync();
+        }
+
+        public async Task<ProductModel?> GetProductByIdAsync(long id)
+        {
+            return await _dbContext.Produtos.FindAsync(id);
+        }
+
+        public async Task RegisterProductAsync(ProductModel product)
+        {
+            _dbContext.Produtos.Add(product);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
