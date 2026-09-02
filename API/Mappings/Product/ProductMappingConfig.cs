94caca8164c993ef9b2c419b75c4138fa5c95a04
Felipe Osorio <felipe.osorio.dev@gmail.com>
2026-09-02 04:46:23 -0300

    feat: Criado classe de configuração do mapster para ignorar valores nullos na converção de dto/entidade de um patch.

---
+++ API/Mappings/Product/ProductMappingConfig.cs
@@ -0,0 +1,16 @@
+﻿using API.DTO.Requests;
+using API.Models.Product;
+using Mapster;
+
+namespace API.Mappings.Product
+{
+    public static class ProductMappingConfig
+    {
+        public static void Register()
+        {
+            TypeAdapterConfig<PatchUpdateProductDTO, ProductModel>
+                .NewConfig()
+                .IgnoreNullValues(true);
+        }
+    }
+}
