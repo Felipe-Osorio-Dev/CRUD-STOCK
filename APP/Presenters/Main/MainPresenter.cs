6e0d9746f90c6fa93b2061e1886ef7d1e4ac1f80
Felipe Osorio <felipe.osorio.dev@gmail.com>
2026-09-03 04:55:29 -0300

    feat: Add classes Presenter para lidar com logicas e regras de negocios das views.

---
+++ APP/Presenters/Main/MainPresenter.cs
@@ -0,0 +1,28 @@
+﻿using APP.Presenters.Register;
+using APP.Services.Navigation;
+using APP.Views.Main;
+using APP.Views.Register;
+
+namespace APP.Presenters.Main
+{
+    internal class MainPresenter
+    {
+        private readonly IMainView _view;
+        private readonly INavigationService _navigationService;
+
+        public MainPresenter(IMainView view, INavigationService navigationService)
+        {
+            _view = view;
+            _navigationService = navigationService;
+
+            _navigationService.SetMainForm((Form) _view);
+
+            _view.RegisterMenuItemClicked += NavigateTo;
+        }
+
+        private void NavigateTo(object sender, EventArgs e)
+        {
+            _navigationService.NavigateTo<RegisterForm, RegisterPresenter>();
+        }
+    }
+}
---
+++ APP/Presenters/Register/RegisterPresenter.cs
@@ -0,0 +1,17 @@
+﻿using APP.Services.Navigation;
+using APP.Views.Register;
+
+namespace APP.Presenters.Register
+{
+    internal class RegisterPresenter
+    {
+        private readonly IRegisterView _registerView;
+        private readonly INavigationService _navigationService;
+
+        public RegisterPresenter(IRegisterView registerView, INavigationService navigationService)
+        {
+            _registerView = registerView;
+            _navigationService = navigationService;
+        }
+    }
+}
