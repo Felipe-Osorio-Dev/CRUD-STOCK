bac228e0e165b7791edc11e48d7ee00bbd6c0f43
Felipe Osorio <felipe.osorio.dev@gmail.com>
2026-09-08 04:55:40 -0300

    feat: Criado classe result para padronizar as respostas e requisições vinda da api.

---
+++ APP/Util/Result/Result.cs
@@ -0,0 +1,30 @@
+﻿namespace APP.Util.Result
+{
+    internal class Result
+    {
+        public bool IsSuccess { get; }
+        public string MessageError { get; }
+
+        protected Result(bool isSuccess, string messageError)
+        {
+            IsSuccess = isSuccess;
+            MessageError = messageError;
+        }
+
+        public static Result Success() => new(true, string.Empty);
+        public static Result Failure(string messageError) => new(false, messageError);
+    }
+
+    internal class Result<T> : Result
+    {
+        public T Data { get; }
+
+        private Result(bool isSuccess, string messageError, T data) : base(isSuccess, messageError)
+        {
+            Data = data;
+        }
+
+        public static Result<T> Success(T data) => new(true, string.Empty, data);
+        public static new Result<T> Failure(string messageError) => new(false, messageError, default!);
+    }
+}
