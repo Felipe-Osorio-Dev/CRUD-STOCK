8ff068c3221adc5be19d31fb1ffcc20191680e88
Felipe Osorio <felipe.osorio.dev@gmail.com>
2026-09-08 04:56:25 -0300

    feat: Criado classes DTOs para envio e recebimento de dados vindo da api.

---
+++ APP/Dtos/Requests/RegisterProductDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace APP.Dtos.Requests
+{
+    public class RegisterProductDTO
+    {
+        public string Name { get; set; }
+        public string Ean { get; set; }
+        public int Amount { get; set; }
+        public DateOnly Validate { get; set; }
+    }
+}
---
+++ APP/Dtos/Responses/CreatedProductDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace APP.Dtos.Responses
+{
+    internal class CreatedProductDTO
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Ean { get; set; }
+        public int Amount { get; set; }
+        public DateOnly Validate { get; set; }
+    }
+}
