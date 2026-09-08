0026af86b09ef3ca54c0e4218a6749f0f81eb255
Felipe Osorio <felipe.osorio.dev@gmail.com>
2026-09-08 04:57:08 -0300

    feat: Criado classe para manipular eventos com dados customizados.

---
+++ APP/Util/CustomArgs/CustomEventArgs.cs
@@ -0,0 +1,12 @@
+﻿namespace APP.Util.CustomArgs
+{
+    public class CustomEventArgs<T> : EventArgs
+    {
+        public T Value { get; }
+
+        public CustomEventArgs(T value)
+        {
+            Value = value;
+        }
+    }
+}
