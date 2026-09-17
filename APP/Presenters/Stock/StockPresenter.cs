cc2802e0e4524ab82b32c1fdac7d915701b54c62
Felipe Osorio <felipe.osorio.dev@gmail.com>
2026-09-17 04:54:12 -0300

    feat: Implementado logica para obtenção dos dados vindo da api.

--- APP/Presenters/Stock/StockPresenter.cs
+++ APP/Presenters/Stock/StockPresenter.cs
@@ -1,13 +1,35 @@
-﻿using APP.Views.Stock;
+﻿using System.ComponentModel;
+using APP.Dtos.Responses;
+using APP.Services.Api.Product;
+using APP.Views.Stock;
 
 namespace APP.Presenters.Stock
 {
     internal class StockPresenter
     {
         private readonly IStockView _view;
-        public StockPresenter(IStockView view)
+        private readonly IProductService _productService;
+
+        public StockPresenter(IStockView view, IProductService productService)
         {
             _view = view;
+            _productService = productService;
+
+            _view.LoadProducts += OnLoadProducts;
+        }
+
+        private async void OnLoadProducts(object sender, EventArgs e)
+        {
+            var result = await _productService.GetAllProductsAsync();
+
+            if(!result.IsSuccess)
+            {
+               MessageBox.Show(result.MessageError);
+               return;
+            }
+
+            _view.Products = new BindingList<ProductDTO>(result.Data);
+            return;
         }
     }
 }
