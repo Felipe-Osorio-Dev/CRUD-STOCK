fd284e0b5c9e2d761862d9592bdad5d73e630deb
Felipe Osorio <felipe.osorio.dev@gmail.com>
2026-09-17 04:52:32 -0300

    feat: Add DTO para dados que ira popular o datagridview.

---
+++ APP/Dtos/Responses/ProductDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace APP.Dtos.Responses
+{
+    internal class ProductDTO
+    {
+        public string Name { get; set; }
+        public string Ean { get; set; }
+        public int Amount { get; set; }
+        public DateOnly Validate { get; set; }
+    }
+}
